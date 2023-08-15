833f1613e255253838bb9ba93e9711c53d8aa2c7
Oriac1337 <126190791+Oriac1337@users.noreply.github.com>
2023-08-15 13:11:33 -0400

    add button to start grid and hoover effect

--- js.js
+++ js.js
@@ -1,17 +1,29 @@
 const container = document.querySelector('#container');
 const button = document.querySelector("#start");
 
-let i = 0;
 button.addEventListener("click", start);
+
 function start() {
+
+    const existingDivs = document.querySelectorAll('.content, .content:hover');
+    existingDivs.forEach(div => container.removeChild(div));
+
 num = prompt(`Enter number of squares per side for the new grid: `)
-while (i < num) {
+while (num > 100) {
+    alert("Invalid input. Please enter a number less than or equal to 100.");
+    num = prompt(`Enter number of squares per side for the new grid: `)
+}
+container.style.setProperty(`--num`, num);
+let i = 0;
+while (i < (num * num)) {
     const newDiv = document.createElement('div');
 		newDiv.classList.add('content');
     container.appendChild(newDiv);
      i++
 }
 };
+
+
 
 window.addEventListener('mouseover', hovering);
 
